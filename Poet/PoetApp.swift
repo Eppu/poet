b1b7ec2af3d056ca0ab6bb150166af1e23febc0e
Eetu Eskelinen <eetuesk@gmail.com>
2022-11-22 17:16:38 +0200

    Initial Commit

---
+++ Poet/PoetApp.swift
@@ -0,0 +1,17 @@
+//
+//  PoetApp.swift
+//  Poet
+//
+//  Created by Eetu Eskelinen on 22.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct PoetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
