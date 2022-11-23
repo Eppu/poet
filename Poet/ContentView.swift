bd1632b7b73a199b7a9f57e5d66a8048d1b648af
Eetu Eskelinen <eetuesk@gmail.com>
2022-11-23 11:43:36 +0200

    Add app delegate and work on content view

--- Poet/ContentView.swift
+++ Poet/ContentView.swift
@@ -7,15 +7,40 @@
 
 import SwiftUI
 
+
 struct ContentView: View {
+    
+    @State private var todoInput: String = ""
+    @FocusState private var todoInputIsFocused: Bool
+    
     var body: some View {
-        VStack {
+        VStack(alignment: .leading) {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
             Text("Hello, world!")
+            
+//           // Populate list with todos
+//            List() { todo in
+//            HStack(alignment: .center) {
+//
+//            }
+                
+//        }
+            TextField(
+                "Todo",
+                text: $todoInput
+            )
+            .focused($todoInputIsFocused)
+            .disableAutocorrection(true)
+            .onSubmit {
+                print("Submitted")
+                todoInput = ""
+            }
         }
         .padding()
+        .frame(width: 300, height: 300)
+        
     }
 }
 
--- Poet/PoetApp.swift
+++ Poet/PoetApp.swift
@@ -5,38 +5,90 @@
 //  Created by Eetu Eskelinen on 22.11.2022.
 //
 
-// Based on https://sarunw.com/posts/swiftui-menu-bar-app/ by Sarun Wongpatcharapakorn.
-
 import SwiftUI
 
 @main
-struct swiftui_menu_barApp: App {
-    @State var currentNumber: String = "1"
+struct PoetApp: App {
+    
+    @NSApplicationDelegateAdaptor(AppDelegate.self) private var appDelegate
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
-        MenuBarExtra(currentNumber, systemImage: "\(currentNumber).circle") {
-            Button("One") {
-                currentNumber = "1"
+    }
+}
+
+class AppDelegate: NSObject, NSApplicationDelegate, ObservableObject {
+    
+    private var statusItem: NSStatusItem!
+    private var popover: NSPopover!
+    
+    @MainActor func applicationDidFinishLaunching(_ notification: Notification) {
+        
+        statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
+        
+        if let statusButton = statusItem.button {
+            // Set status bar image
+            statusButton.image = NSImage(systemSymbolName: "1.circle", accessibilityDescription: "Circle with number 1")
+            
+            statusButton.action = #selector(togglePopover)
+        }
+            
+        self.popover = NSPopover()
+        self.popover.contentSize = NSSize(width: 300, height: 300)
+        self.popover.behavior = .transient
+        self.popover.contentViewController = NSHostingController(rootView: ContentView())
+    }
+    
+    @objc func togglePopover() {
+        if let button = statusItem.button {
+            if popover.isShown {
+                self.popover.performClose(nil);
+            } else {
+                popover.show(relativeTo: button.bounds, of: button, preferredEdge: NSRectEdge.minY)
             }
-            .keyboardShortcut("1")
-            Button("Two") {
-                currentNumber = "2"
-            }
-            .keyboardShortcut("2")
-            Button("Three") {
-                currentNumber = "3"
-            }
-            .keyboardShortcut("3")
-            Divider()
-
-            Button("Quit") {
-
-                NSApplication.shared.terminate(nil)
-
-            }.keyboardShortcut("q")
-
         }
     }
 }
+
+
+
+
+
+
+// macOS 13 implementation Based on https://sarunw.com/posts/swiftui-menu-bar-app/ by Sarun Wongpatcharapakorn.
+
+//import SwiftUI
+
+//@main
+//struct swiftui_menu_barApp: App {
+//    @State var currentNumber: String = "1"
+//    var body: some Scene {
+//        WindowGroup {
+//            ContentView()
+//        }
+//        MenuBarExtra(currentNumber, systemImage: "\(currentNumber).circle") {
+//            Button("One") {
+//                currentNumber = "1"
+//            }
+//            .keyboardShortcut("1")
+//            Button("Two") {
+//                currentNumber = "2"
+//            }
+//            .keyboardShortcut("2")
+//            Button("Three") {
+//                currentNumber = "3"
+//            }
+//            .keyboardShortcut("3")
+//            Divider()
+
+//            Button("Quit") {
+//
+//                NSApplication.shared.terminate(nil)
+//
+//            }.keyboardShortcut("q")
+//
+//        }
+//    }
+//}
